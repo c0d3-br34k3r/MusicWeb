862c36edf7f6b48677278a81bf05d325f02d7232
Your Name <aritroghosh4@gmail.com>
2021-05-19 01:33:32 +0530

    update

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require ("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/myrecords" , {
+mongoose.connect("mongodb+srv://aritro:aritro@cluster0.7l5by.mongodb.net/myrecords" , {
     useNewUrlParser:true,
     useUnifiedTopology:true,                           
     useCreateIndex:true
